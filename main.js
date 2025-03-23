52732743e8d2e450174cf1cad32809afe23f7245
OSLiberation <open-source-liberation@proton.me>
2025-03-23 00:15:19 -0400

    Updates to the page

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     
-document.getElementById("sinceStart").innerHTML = `There have been ${Math.floor((currentDate - termStart) / MILLISECONDS_IN_A_DAY)} days since the start of the Trump presidency.`
-document.getElementById("untilOver").innerHTML = `There are  ${Math.floor((termEnd - currentDate) / MILLISECONDS_IN_A_DAY)} days left.`
+// document.getElementById("sinceStart").innerHTML = `There have been ${Math.floor((currentDate - termStart) / MILLISECONDS_IN_A_DAY)} days since the start of the Trump presidency.`
+// document.getElementById("untilOver").innerHTML = `There are  ${Math.floor((termEnd - currentDate) / MILLISECONDS_IN_A_DAY)} days left.`
 }
 
 const MILLISECONDS_IN_A_DAY = 1000 * 60 * 60 * 24
