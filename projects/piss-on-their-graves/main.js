c576f3bacca29b17684b4087afde167d9317585c
OSLiberation <open-source-liberation@proton.me>
2025-04-03 20:51:37 -0400

    updates

--- projects/piss-on-their-graves/main.js
+++ projects/piss-on-their-graves/main.js
@@ -2,6 +2,7 @@ window.onload = function(){
     var people = document.getElementById('people')
     var results = []
 
+    console.log(screen.width)
     fetch('/projects/piss-on-their-graves/data.json').then(response => response.json()).then(json=>{
         //console.log(people)
         json.forEach(person => {
@@ -14,28 +15,66 @@ window.onload = function(){
             element.onclick = ()=>{
                 console.log(person)
                 var details = document.createElement('div')
+                // Header
+                var header = document.createElement('h2')
+
+                // Tags
+                var tags = document.createElement('div')
+                tags.id = 'tags'
+
+                person.tags.forEach(tag => {
+                    let t = document.createElement('div')
+                    t.innerHTML = tag
+                    t.className = 'tag'
+                    tags.innerHTML += t.outerHTML
+                })
                 
-                document.getElementById('details').innerHTML = person.name
+                // Gravesite
+                var gravesite = document.createElement('div')
+                var image = document.createElement('img')
+                var hr = document.createElement('hr')
+
+                image.src = '/projects/piss-on-their-graves/tombstone.png'
+                hr.className = 'divider'
+                gravesite.id = 'gravesite'
+                
+                gravesite.innerHTML += image.outerHTML + ' '+ person.gravesite + hr.outerHTML
+
+                // Crimes
+                var crimes = document.createElement('div')
+                crimes.id = 'crimes'
+                crimes.innerHTML += "Known for..."
+
+                person.crimes.forEach(crime => {
+                    let c = document.createElement('div')
+                    let title = document.createElement('p')
+                    let description = document.createElement('p')
+
+                    title.className = 'title'
+                    description.className = 'description'
+                    title.innerHTML = crime.title
+                    description.innerHTML = crime.description
+
+                    c.innerHTML = title.outerHTML
+                    c.innerHTML += description.outerHTML
+
+                    crimes.innerHTML += c.outerHTML
+                })
+                
+                // 
+                header.innerText = person.name
+                details.innerHTML = header.outerHTML
+                details.innerHTML += tags.outerHTML
+                details.innerHTML += gravesite.outerHTML
+                details.innerHTML += crimes.outerHTML
+                
+                document.getElementById('details').innerHTML = details.outerHTML
             }
             
             results.push(element.outerHTML)
             
             people.appendChild(element)
         });
-        // <div
-        //     key={p.name}
-        //     className="person"
-        //     onClick={() => {
-        //       setState({ ...state, screen: "details", selected: p.name });
-        //     }}
-        //     style={{
-        //       backgroundImage: `url(./portraits/${p.image})`,
-        //       backgroundColor: "grey",
-        //       filter: "grayscale(100%)",
-        //       borderColor: `${p.name === state.selected ? "red" : "blue"}`,
-        //     }}
-        //   ></div>
-        // people.innerHTML = results
     })
 }
 
